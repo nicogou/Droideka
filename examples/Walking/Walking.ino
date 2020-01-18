2168f318844069881dee365ca9ab2755c30b186f
nicogou <33235852+nicogou@users.noreply.github.com>
2020-01-18 14:53:41 +0100

    minor changes in example files

--- examples/Walking/Walking.ino
+++ examples/Walking/Walking.ino
@@ -12,6 +12,9 @@ Droideka *droid_1;
 int time = 500;
 int time_offset = 2000;
 
+float parking[LEG_NB][3] = {{90.0, 9.8, 9.0}, {90, 9.8, 9.0}, {90, 9.8, 9.0}, {90, 9.8, 9.0}};
+Droideka_Position parking_pos(parking);
+
 void setup()
 {
     Serial.begin(9600);
@@ -19,8 +22,11 @@ void setup()
     Serial2.begin(DEBUG_BOARD_BAUD_RATE);
     droid_1 = new Droideka(&Serial2, &Serial1);
     droid_1->initialize(LMP1, LMP2, LMP_PWM, REC_RX, REC_TX, REC_STATE);
+    droid_1->set_parking_position(&parking_pos);
+
     Serial.println("Start");
     droid_1->walk(5);
+    Serial.println(droid_1->park(true, 2000, 2000));
 }
 
 void loop()
