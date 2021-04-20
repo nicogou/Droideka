68e014795d5d07ed4f43e89f1095b41b312bde91
nicogou <33235852+nicogou@users.noreply.github.com>
2021-04-20 18:25:59 +0200

    Removed unnecessary vars

--- Droideka.h
+++ Droideka.h
@@ -72,12 +72,7 @@ public:
 
     // orientation/motion vars
     Quaternion q;        // [w, x, y, z]         quaternion container
-    VectorInt16 aa;      // [x, y, z]            accel sensor measurements
-    VectorInt16 gy;      // [x, y, z]            gyro sensor measurements
-    VectorInt16 aaReal;  // [x, y, z]            gravity-free accel sensor measurements
-    VectorInt16 aaWorld; // [x, y, z]            world-frame accel sensor measurements
     VectorFloat gravity; // [x, y, z]            gravity vector
-    float euler[3];      // [psi, theta, phi]    Euler angle container
     float ypr[3];        // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
     Droideka(HardwareSerial *serial_servos, int8_t tXpin_servos, int8_t rx, int8_t tx, int16_t thresh[NB_MAX_DATA * 2], String btHardware, int8_t l_m_p_1, int8_t l_m_p_2, int8_t l_m_p_pwm, int8_t imu_int_pin);            // Class constructor.
